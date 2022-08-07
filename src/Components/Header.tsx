d59c3ea9788b5a50f32feccdfdab6387bc9bd8be
kelo221 <villekum@metropolia.f>
2022-08-07 20:45:17 +0300

    Header style alt

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,13 +1,41 @@
 import React from "react";
-import { Box } from "grommet";
+import { Text, Box, Clock, Grid } from "grommet/es6";
 
+const columns = [
+  ["xxsmall", "xsmall"], // column 1 is able to flex
+  ["xsmall", "small"], // column 2 is able to flex
+  ["xxsmall", "xsmall"], // column 1 is able to flex
+];
 function MainHeader() {
   return (
-    <React.Fragment>
-      <Box tag="header" background="brand" pad="small" align="center">
-        Nokia One Cafeteria Menu
-      </Box>
-    </React.Fragment>
+    <Box>
+      <Grid
+        fill
+        rows={["auto", "flex"]}
+        columns={["auto", "flex"]}
+        areas={[
+          { name: "header", start: [0, 0], end: [1, 0] },
+          { name: "main", start: [1, 1], end: [1, 1] },
+        ]}
+      >
+        <Box
+          gridArea="header"
+          direction="row"
+          align="center"
+          justify="between"
+          pad={{ horizontal: "medium", vertical: "small" }}
+          background="brand"
+        >
+          <Text>Nokia One Cafeteria Menu</Text>
+          <Clock type="digital" />
+        </Box>
+      </Grid>
+
+      {/*<Box tag="header" background="brand" pad="small" align="center">
+        <Text>Nokia One Cafeteria Menu</Text>
+        <Clock type="digital" />
+      </Box>*/}
+    </Box>
   );
 }
 
